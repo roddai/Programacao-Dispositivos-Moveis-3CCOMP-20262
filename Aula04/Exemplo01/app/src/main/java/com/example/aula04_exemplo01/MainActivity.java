2f42a65e10bc65d0e7051e3941879e79dfd12c9c
Guilherme Augusto Castilho <25028100@fecap.br>
2026-08-27 21:28:45 -0300

    Botões funcionando

--- Aula04/Exemplo01/app/src/main/java/com/example/aula04_exemplo01/MainActivity.java
+++ Aula04/Exemplo01/app/src/main/java/com/example/aula04_exemplo01/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula04_exemplo01;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    Livro livro1, livro2, livro3;
+    TextView textInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        livro1 = new Livro("Desenvolvendo seu primeiro APP Android", "Autor 1", 2021);
+        livro2 = new Livro("Programação de Jogos Android", "Autor 2", 2022);
+        livro3 = new Livro("Dev de aplivtivos", "Autor 3", 2023);
+
+        textInfo = findViewById(R.id.textInfo);
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v -> textInfo.setText(livro1.getInfo()));
+        button2.setOnClickListener(v -> textInfo.setText(livro2.getInfo()));
+        button3.setOnClickListener(v -> textInfo.setText(livro3.getInfo()));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
