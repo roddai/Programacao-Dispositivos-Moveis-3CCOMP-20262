9de62565e466b31a74e6a04d672422b66bcb77ab
Lucas de Freitas Soares <25028169@fecap.br>
2026-09-03 21:53:35 -0300

    Aula 05

---
+++ Aula05/app/src/main/java/com/example/aula05/ResultadoAbaixoPeso.java
@@ -0,0 +1,32 @@
+package com.example.aula05;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultadoAbaixoPeso extends AppCompatActivity {
+
+    Button btnVoltar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_abaixo_peso);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+    }
+}
