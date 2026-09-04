91c6eec33d9571e1dc612cd2154b96941a800cb6
Rodrigo da Rosa <27532544869@fecap.br>
2026-09-03 22:01:29 -0300

    finaliza telas do exemplo da aula 5

--- Aula05/app/src/main/java/com/example/projetoaula05/ResultadoObesidade2.java
+++ Aula05/app/src/main/java/com/example/projetoaula05/ResultadoObesidade2.java
@@ -1,4 +1,34 @@
 package com.example.projetoaula05;
 
-public class ResultadoObesidade2 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoObesidade2 extends AppCompatActivity {
+
+    Button btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_obesidade_2);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
