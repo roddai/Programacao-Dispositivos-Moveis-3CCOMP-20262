e8e9e4235233c64b22d5b55fa1d95ed7977073a1
Fernanda Figueiredo Simões <25028062@fecap.br>
2026-09-03 22:25:26 -0300

    Projeto aula 05

---
+++ Aula05/app/src/main/java/com/example/projetoaula05/ResultadoObesidade2.java
@@ -0,0 +1,40 @@
+package com.example.projetoaula05;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoObesidade2 extends AppCompatActivity {
+
+    Button btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_obesidade_2);
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+
+            });
+
+
+    }
+
+
+
+
+        }
+
