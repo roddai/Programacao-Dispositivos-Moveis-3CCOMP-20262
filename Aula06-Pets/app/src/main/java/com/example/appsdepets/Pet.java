d448417a000e81f39db62268218f9e26195d6bde
Emilly Oliveira dos Santos <25028171@fecap.br>
2026-09-10 19:49:29 -0300

    Commit 1 - crCação da classe Pet

---
+++ Aula06-Pets/app/src/main/java/com/example/appsdepets/Pet.java
@@ -0,0 +1,18 @@
+package com.example.appsdepets;
+
+public class Pet {
+    String nome;
+    String cor;
+    String raca;
+    int idade;
+    String aniversario;
+
+    public Pet (String n, String c, String r, int i, String niver){
+        this.nome = n;
+        this.cor = c;
+        this.raca = r;
+        this.idade = i;
+        this.aniversario = niver;
+    }
+
+}
