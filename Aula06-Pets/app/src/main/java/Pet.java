634502204a042cdaa4b13ab3399fcf87c2fbf799
Julia Valério da Silva <25028281@fecap.br>
2026-09-10 20:15:02 -0300

    Criação da classe Pet

---
+++ Aula06-Pets/app/src/main/java/Pet.java
@@ -0,0 +1,29 @@
+public class Pet {
+
+    private String nome;
+
+    private String cor;
+
+
+    private String raca;
+
+
+    private int idade;
+
+
+    private int dataDeAniversario;
+
+    public Pet(String nome, String cor, String raca, int idade, int dataDeAniversario){
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.dataDeAniversario = dataDeAniversario;
+    }
+
+    public String getInfo(){
+        return "Nome: " + nome + "\nCor: " + cor + "\nRaça" + raca + "\nIdade: " + idade + "\nData de Aniversário: " + dataDeAniversario;
+    }
+
+
+}
