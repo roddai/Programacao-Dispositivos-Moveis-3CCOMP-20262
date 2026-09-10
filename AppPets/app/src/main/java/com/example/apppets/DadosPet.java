695aec62e510540eb2e8c9165b66ab718a5d0d0d
Kaike Cavalcante Santos <25028147@fecap.br>
2026-09-10 20:11:05 -0300

    criação da classe Pet

---
+++ AppPets/app/src/main/java/com/example/apppets/DadosPet.java
@@ -0,0 +1,26 @@
+package com.example.apppets;
+
+import java.util.Date;
+
+public class DadosPet {
+
+   private String Nome;
+   private String Cor;
+   private String Raca;
+   private int Idade;
+   private Date Aniversario;
+
+
+   public DadosPet (String Nome, String Raca, String Cor, int Idade, Date Aniversario){
+   this.Nome = Nome;
+   this.Raca = Raca;
+   this.Cor = Cor;
+   this.Idade = Idade;
+   this.Aniversario = Aniversario;
+
+   }
+   public String getInfo(){
+       return "Nome: " + Nome + "\nRaça: " + Raca + "\nCor: " + Cor + "\nIdade: " + Idade + "\nAniversário: " + Aniversario;
+   }
+
+}
