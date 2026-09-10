0ae4885fb9c2af3623c9a453e20241582fc27e9c
Luiz Roberto Silvestre <luizrsilvestre@gmail.com>
2026-09-10 19:59:13 -0300

    criação da classe Pet

---
+++ Aula06-Pets/app/src/main/java/com/example/myapplication/Pet.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+public class Pet {
+    public  String nome;
+    public String cor;
+    public String raca;
+    public int idade;
+    public String aniversario;
+    public Pet (String nome,String cor,String raca,int idade,String aniversario){
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.aniversario = aniversario;
+    }
+}
