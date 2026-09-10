c0c6c1eacb87b9c18f5275123b4f56ba53f2c024
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-10 19:56:49 -0300

    criação da classe Pet

---
+++ Aula06/app/src/main/java/com/example/aula06desafio/Pet.java
@@ -0,0 +1,17 @@
+package com.example.aula06desafio;
+
+public class Pet {
+    public String nome;
+    public String cor;
+    public String raca;
+    public int idade;
+    public String aniversario;
+
+    public Pet(String nomePet, String corPet, String racaPet, int idadePet, String aniversarioPet){
+        this.nome = nomePet;
+        this.cor = corPet;
+        this.raca = racaPet;
+        this.idade = idadePet;
+        this.aniversario = aniversarioPet;
+    }
+}
