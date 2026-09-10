f195703bb18eff2697df86a77767fadc2f6f7de5
Hellen Caroline Batista Silva <23025372@fecap.br>
2026-09-10 19:56:14 -0300

    Criação da Classe Pet

---
+++ Aula06_Exercicio/app/src/main/java/com/example/aula06_exercicio/Pet.java
@@ -0,0 +1,23 @@
+package com.example.aula06_exercicio;
+
+public class Pet {
+
+    public String nome;
+    public String cor;
+    public String raca;
+    public Integer idade;
+    public Integer aniversario;
+
+    public Pet(String nome, String cor, String raca, Integer idade, Integer aniversario){
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.aniversario = aniversario;
+    }
+
+
+
+
+
+}
