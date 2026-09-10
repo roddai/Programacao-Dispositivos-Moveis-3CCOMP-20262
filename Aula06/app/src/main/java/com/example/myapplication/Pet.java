ad7ded6bfd5c174aee07a637309a9be4178b57e6
Fernanda Figueiredo Simões <25028062@fecap.br>
2026-09-10 19:53:30 -0300

    criação da classe Pet

---
+++ Aula06/app/src/main/java/com/example/myapplication/Pet.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+public class Pet {
+    private String nome;
+    private String cor;
+    private String raça;
+    private int idade;
+    private String aniversario;
+    public Pet(String nome, String cor, String raça, int idade, String aniversario ) {
+    this.nome = nome;
+    this.cor = cor;
+    this.raça = raça;
+    this.idade = idade;
+    this.aniversario = aniversario;
+}
+public String getInfo(){
+        return "Nome: " + nome + "\nCor:" + cor + "\nRaça:" + raça + "\nIdade:" + idade + "\nAniversario:" + aniversario;
+    }
+}
+
