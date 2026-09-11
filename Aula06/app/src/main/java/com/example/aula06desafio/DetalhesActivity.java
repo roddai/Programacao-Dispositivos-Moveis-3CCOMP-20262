afc9fae5ee7baf07d32335667e82f0bb3268c7dd
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-09-10 21:45:53 -0300

    criação do layout principal

---
+++ Aula06/app/src/main/java/com/example/aula06desafio/DetalhesActivity.java
@@ -0,0 +1,4 @@
+package com.example.aula06desafio;
+
+public class DetalhesActivity {
+}
--- Aula06/app/src/main/java/com/example/aula06desafio/MainActivity.java
+++ Aula06/app/src/main/java/com/example/aula06desafio/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aula06desafio;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2, btn3, btn4, btn5, btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
