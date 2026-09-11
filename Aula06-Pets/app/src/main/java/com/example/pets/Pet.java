9ed6f2d82e9205b93f1f4bb6c0ad06b8b0e1549f
Guilherme Augusto Castilho <25028100@fecap.br>
2026-09-10 22:19:25 -0300

    Commit4-Desenvolvimento da classe principal

--- Aula06-Pets/app/src/main/java/com/example/pets/Pet.java
+++ Aula06-Pets/app/src/main/java/com/example/pets/Pet.java
@@ -7,13 +7,13 @@ public class Pet {
     private int idade;
     private String aniversario;
 
-    Pet(String nome,String cor,String raca,int idade,String aniversario){
+    public Pet(String nome,String cor,String raca,int idade,String aniversario){
         this.nome = nome;
         this.cor = cor;
         this.raca = raca;
         this.idade = idade;
         this.aniversario = aniversario;
-    };
+    }
 
     public String getInfo() {
         return "Nome: "+ nome + "\nCor: "+cor + "\nRaca: " + raca + "\nIdade: " + idade +"\nAniversário: " +aniversario;
