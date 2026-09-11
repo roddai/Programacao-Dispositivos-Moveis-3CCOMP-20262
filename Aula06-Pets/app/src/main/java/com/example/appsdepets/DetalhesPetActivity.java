5865e6f7e032c187741cd43b1edbcaf202bdb475
Emilly Oliveira dos Santos <25028171@fecap.br>
2026-09-10 21:21:11 -0300

    Commit 4 - Desenvolvimento da classe principal (com erros - put extra)

--- Aula06-Pets/app/src/main/java/com/example/appsdepets/DetalhesPetActivity.java
+++ Aula06-Pets/app/src/main/java/com/example/appsdepets/DetalhesPetActivity.java
@@ -1,5 +1,19 @@
 package com.example.appsdepets;
 
-public class DetalhesPetActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-}
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetalhesPetActivity extends AppCompatActivity {
+
+
+
+    }
--- Aula06-Pets/app/src/main/java/com/example/appsdepets/MainActivity.java
+++ Aula06-Pets/app/src/main/java/com/example/appsdepets/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.appsdepets;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,18 +14,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button detalhe1, detalhe2, detalhe3;
-    Button detalhec1, detalhec2, detalhec3;
+    Pet cao1, cao2, cao3;
+    Pet gato1, gato2, gato3;
+
+    TextView textInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        /* Instância dawgs*/
+        cao1 = new Pet("ThorWhite", "branco com manchas pretas", "Bull Terrier", 3, "10/09/2026" );
+        gato1 = new Pet("Daminha", "branco", "Persa", 2, "11/09/2026" );
+
+
+
+
+        Button cao1 = findViewById(R.id.detalhe1);
+        Button detalhec1 = findViewById(R.id.detalhec1);
+
+        cao1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String info = textInfo.getText().toString();
+
+                Intent segundaTela = new Intent(MainActivity.this, DetalhesPetActivity.class);
+                segundaTela.putExtra(detalhes, detalhesRecebido);
+                startActivity(segundaTela);
+            }
         });
+
     }
 }
