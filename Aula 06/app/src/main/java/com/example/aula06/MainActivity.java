0dc540d0a072ac4c654a0a4f5db62248ccacf53b
Enzo Yuichi Milen Sakita <25027795@fecap.br>
2026-09-10 22:10:11 -0300

    criação do layout da tela de detalhes - Aula 06

--- Aula 06/app/src/main/java/com/example/aula06/MainActivity.java
+++ Aula 06/app/src/main/java/com/example/aula06/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aula06;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    public Pet Thor = new Pet ("Thor", "Dourado", "Golden Retriever", "22/05/2024", 3);
+    public Pet Rex = new Pet("Rex", "Preto", "Pinsher", "12/08/2025", 1);
+    public Pet Luna = new Pet("Luna", "Dourado", "raça3", "02/03/2025",1);
+    public Pet Olaf = new Pet("Olaf", "Brando", "raça4", "07/09/2025", 1);
+    public Pet Simba = new Pet("Simba", "Laranja", "raça5", "22/03/2023", 3);
+    public Pet Chico = new Pet("Chico", "Branco e Preto", "raça6", "10/01/2025", 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View -> v){
+                Intent detalhes = new Intent(MainActivity.this, Pet.class);
+                detalhes.putExtra("nome", Thor);
+                startActivity(detalhes);
+            }
+        }
         });
     }
 }
