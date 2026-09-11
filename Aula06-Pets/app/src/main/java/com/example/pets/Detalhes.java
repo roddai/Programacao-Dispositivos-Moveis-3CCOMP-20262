3dba889d22aa6e78a557bbda25c036eabafb1880
Guilherme Augusto Castilho <25028100@fecap.br>
2026-09-10 22:32:05 -0300

    Commit5-Desenvolvimento da classe detalhes

--- Aula06-Pets/app/src/main/java/com/example/pets/Detalhes.java
+++ Aula06-Pets/app/src/main/java/com/example/pets/Detalhes.java
@@ -1,4 +1,25 @@
 package com.example.pets;
 
-public class Detalhes {
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Detalhes extends AppCompatActivity{
+
+    private TextView txtDetalhes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes); // Layout da segunda tela
+
+        txtDetalhes = findViewById(R.id.detalhes);
+
+        // Pegando o nome enviado da MainActivity
+        String nomeRecebido = getIntent().getStringExtra("info");
+
+        txtDetalhes.setText("Olá, " + nomeRecebido + "!");
+    }
+
+
 }
