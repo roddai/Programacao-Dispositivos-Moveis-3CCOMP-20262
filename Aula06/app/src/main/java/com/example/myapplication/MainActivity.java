60b064005e61a6c32f5ddf615653d5aac8dd8fbf
Fernanda Figueiredo Simões <25028062@fecap.br>
2026-09-10 21:58:59 -0300

    desenvolvimento da classe principal

--- Aula06/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Aula06/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Pet cao1, cao2, cao3;
+    Pet gato1, gato2, gato3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        cao1 = new Pet("Bob", "Laranja", "Golden", 7, "10 de maio");
+        cao2 = new Pet("Thor", "Marrom", "Labrador", 3, "8 de abril");
+        cao3 = new Pet("Mel", "Caramelo", "Caramelo", 10, " 20 de setembro");
+
+        gato1 = new Pet("Luna", "Cinza", "Persa", 1, "15 de março");
+        gato2 = new Pet("Simba", "Amarelo", "Siamês", 2, "10 de janeiro");
+        gato3 = new Pet("Nina", "Marrom", "Angorá", 5, "2 de dezembro");
+
+
+        Button btnCao1 = findViewById(R.id.btnCao1);
+        Button btnCao2 = findViewById(R.id.btnCao2);
+        Button btnCao3 = findViewById(R.id.btnCao3);
+
+        Button btnGato1 = findViewById(R.id.btnGato1);
+        Button btnGato2 = findViewById(R.id.btnGato2);
+        Button btnGato3 = findViewById(R.id.btnGato3);
+        btnCao1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irParaSegundaTela = new Intent(MainActivity.this, Detalhes.class);
+                irParaSegundaTela.putExtra("info", cao1.getInfo());
+                startActivity(irParaSegundaTela);
+            }
         });
     }
 }
