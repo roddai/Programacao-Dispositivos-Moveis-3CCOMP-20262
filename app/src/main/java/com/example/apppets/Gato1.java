a37a63b970db5cddfc0580ca83a8078fa6f7c8bb
Michaelcondor <Michaelcomdori90@gmail.com>
2026-09-11 02:33:23 -0300

    criação do layout da tela de detalhes e criação do pet.java

--- app/src/main/java/com/example/apppets/Gato1.java
+++
@@ -1,4 +0,0 @@
-package com.example.apppets;
-
-public class Gato1 {
-}
--- app/src/main/java/com/example/apppets/MainActivity.java
+++ app/src/main/java/com/example/apppets/MainActivity.java
@@ -9,31 +9,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    public class pet {
 
+    private Pet rex, luna, skibi, oggy, tom, larry;
 
-    private String nome, cor, raca, aniversario;
-    private int idade;
-    public pet(String nome,String cor,String raca,String aniversario, int idade){
-        this.nome = nome;
-        this.cor = cor;
-        this.raca = raca;
-        this.aniversario = aniversario;
-        this.idade = idade;
-
-    }
-
-
-}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        rex = new Pet("Rex", "Marrom", "Labrador", "10/05/2023", 3);
+        luna = new Pet("Luna", "Branca", "Poodle", "15/08/2023", 2);
+        skibi = new Pet("Skibi", "Preto", "Vira-lata", "20/02/2024", 1);
+
+        oggy = new Pet("Oggy", "Cinza", "Siamês", "12/01/2022", 4);
+        tom = new Pet("Tom", "Laranja", "Persa", "18/06/2023", 3);
+        larry = new Pet("Larry", "Branco", "Angorá", "05/09/2023", 2);
     }
 }
