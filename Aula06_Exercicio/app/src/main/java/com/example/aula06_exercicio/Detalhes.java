6df7385011067f9ac2b7621526034dd7a0ff2574
Lucas De Carvalho Carretero <26029787@fecap.br>
2026-09-10 22:27:30 -0300

    finaliza projeto v2 - atualização de estilização

--- Aula06_Exercicio/app/src/main/java/com/example/aula06_exercicio/Detalhes.java
+++ Aula06_Exercicio/app/src/main/java/com/example/aula06_exercicio/Detalhes.java
@@ -1,6 +1,7 @@
 package com.example.aula06_exercicio;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Detalhes extends AppCompatActivity {
 
     public TextView detalhePet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detalhe_pet); // Layout da segunda tela
 
         detalhePet = findViewById(R.id.detalhePet);
+
 
         // Pegando o nome enviado da MainActivity
         String detalheRecebido = getIntent().getStringExtra("detalhePet");
