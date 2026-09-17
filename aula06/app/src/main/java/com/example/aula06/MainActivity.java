1941ffff688b825626168b98a69ed830c13ddcda
25028261 <Michaelcomdori90@gmail.com>
2026-09-17 19:44:13 -0300

    Salvando alterações locais

---
+++ aula06/app/src/main/java/com/example/aula06/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.aula06;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    textView  txtResultado;
+
+    ImageView imgMaquina, imgPedra, imgPapel, imgTesoura;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        txtResultado = findViewById(R.id.txtResultado);
+        imgMaquina = findViewById(R.id.imgMaquina);
+        imgPedra = findViewById(R.id.imgPedra);
+        imgPapel = findViewById(R.id.imgPapel);
+        imgTesoura = findViewById(R.id.imgTesoura);
+
+    }
+}
