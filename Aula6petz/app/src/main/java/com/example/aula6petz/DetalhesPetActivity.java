05ec64192eafd807cad23624bbaec9c501ae4d06
Arthur Daher Franceschelli <25028245@fecap.br>
2026-09-17 21:41:49 -0300

    desenvolvimento da classe dos pets 5

--- Aula6petz/app/src/main/java/com/example/aula6petz/DetalhesPetActivity.java
+++ Aula6petz/app/src/main/java/com/example/aula6petz/DetalhesPetActivity.java
@@ -1,4 +1,41 @@
 package com.example.aula6petz;
 
-public class DetalhesPetActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetalhesPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detalhes);
+
+        TextView txtNome = findViewById(R.id.txtNome);
+        TextView txtCor = findViewById(R.id.txtCor);
+        TextView txtRaca = findViewById(R.id.txtRaca);
+        TextView txtIdade = findViewById(R.id.txtIdade);
+        TextView txtAniversario = findViewById(R.id.txtAniversario);
+
+        ImageView imgPet = findViewById(R.id.imgPet);
+
+        String nome = getIntent().getStringExtra("nome");
+        String cor = getIntent().getStringExtra("cor");
+        String raca = getIntent().getStringExtra("raca");
+        int idade = getIntent().getIntExtra("idade", 0);
+        String aniversario = getIntent().getStringExtra("aniversario");
+
+        int imagem = getIntent().getIntExtra("imagem", 0);
+
+        txtNome.setText("Nome: " + nome);
+        txtCor.setText("Cor: " + cor);
+        txtRaca.setText("Raça: " + raca);
+        txtIdade.setText("Idade: " + idade + " anos");
+        txtAniversario.setText("Aniversário: " + aniversario);
+
+        imgPet.setImageResource(imagem);
+    }
 }
