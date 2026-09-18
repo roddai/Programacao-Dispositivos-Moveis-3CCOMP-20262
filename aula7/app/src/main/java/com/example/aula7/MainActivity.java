d3a2993755c53e1974fa24de2c0a35d4ef49c1eb
Ryan Santos <ryan.santos92508@gmail.com>
2026-09-17 22:23:51 -0300

    Aula 7 parte 2 finalizado

--- aula7/app/src/main/java/com/example/aula7/MainActivity.java
+++ aula7/app/src/main/java/com/example/aula7/MainActivity.java
@@ -12,7 +12,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     TextView txtResultado;
-    ImageView imgMaquina, imgPedra, imgPapel, imgTesoura;
+
+    ImageView imgMaquina, imgInterrogacao, imgPedra, imgPapel, imgTesoura;
+    ImageView imgUsuario, imgUsuarioEscolha;
+    ImageView imgUsuarioFem, imgUsuarioMasc;
+
+    String personagemEscolhido = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +26,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtResultado = findViewById(R.id.txtResultado);
+
         imgMaquina = findViewById(R.id.imgMaquina);
+        imgInterrogacao = findViewById(R.id.imgInterrogacao);
+
         imgPedra = findViewById(R.id.imgPedra);
-        imgTesoura = findViewById(R.id.imgTesoura);
         imgPapel = findViewById(R.id.imgPapel);
+        imgTesoura = findViewById(R.id.imgTesoura);
+
+        imgUsuario = findViewById(R.id.imgUsuario);
+        imgUsuarioEscolha = findViewById(R.id.imgUsuarioEscolha);
+
+        imgUsuarioFem = findViewById(R.id.imgUsuarioFem);
+        imgUsuarioMasc = findViewById(R.id.imgUsuarioMasc);
 
         imgPedra.setOnClickListener(v -> jogar("pedra"));
         imgPapel.setOnClickListener(v -> jogar("papel"));
         imgTesoura.setOnClickListener(v -> jogar("tesoura"));
+
+        imgUsuarioFem.setOnClickListener(v -> {
+            personagemEscolhido = "fem";
+            imgUsuario.setImageResource(R.drawable.usuario_fem);
+        });
+
+        imgUsuarioMasc.setOnClickListener(v -> {
+            personagemEscolhido = "masc";
+            imgUsuario.setImageResource(R.drawable.usuario_masc);
+        });
     }
 
     public void jogar(String escolhaUsuario) {
+
+        if (personagemEscolhido.isEmpty()) {
+            txtResultado.setText("Escolha um personagem primeiro!");
+            return;
+        }
+
+        switch (escolhaUsuario) {
+            case "pedra":
+                imgUsuarioEscolha.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel":
+                imgUsuarioEscolha.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imgUsuarioEscolha.setImageResource(R.drawable.tesoura);
+                break;
+        }
 
         String[] opcoes = {"pedra", "papel", "tesoura"};
         int numero = new Random().nextInt(3);
         String escolhaMaquina = opcoes[numero];
 
         switch (escolhaMaquina) {
-
             case "pedra":
-                imgMaquina.setImageResource(R.drawable.pedra);
+                imgInterrogacao.setImageResource(R.drawable.pedra);
                 break;
 
             case "papel":
-                imgMaquina.setImageResource(R.drawable.papel);
+                imgInterrogacao.setImageResource(R.drawable.papel);
                 break;
 
             case "tesoura":
-                imgMaquina.setImageResource(R.drawable.tesoura);
+                imgInterrogacao.setImageResource(R.drawable.tesoura);
                 break;
         }
 
