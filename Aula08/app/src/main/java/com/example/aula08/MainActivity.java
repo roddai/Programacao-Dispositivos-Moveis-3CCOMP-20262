d1157ed7d8f1bdc460e70360bb8925f31548f070
Emilly Oliveira dos Santos <25028171@fecap.br>
2026-09-24 20:38:14 -0300

    Aula 08 - Conceitos novos

---
+++ Aula08/app/src/main/java/com/example/aula08/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.aula08;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnProximo = findViewById(R.id.btnProximo);
+        btnProximo.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FormActivity.class);
+            startActivity(intent);
+        });
+    }
+}
+
